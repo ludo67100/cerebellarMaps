c7a0d8fad3c8ba876367ff2b9e20db918bbcc207
jyotikab <jyotika.bahuguna@gmail.com>
2021-03-28 12:10:22 +0200

    Corrected the raw data directory

--- Adaptive Dataset/Read_maps/read_data.py
+++ Adaptive Dataset/Read_maps/read_data.py
@@ -12,7 +12,7 @@ import h5py
 import pandas as pd
 
 # Read the raw data from here
-data_dir = "/home/bahuguna/Work/Isope_data/Isope_data_cerebellar_maps/"
+data_dir = "../../COMPLETE_DATASET/For\\ Paper/EPHYS/Adaptive_Dataset/"
 # Store processed data here
 data_target_dir = "../../data/"
 fig_target_dir = "../../figs/"
@@ -27,7 +27,8 @@ zone_lims = [(-233,-133),(-133,-108),(-108,-58),(-58,0),(0,50),(50,100),(100,125
 data = pd.DataFrame()
 
 # All adaptive types = CT, EC, LC, LS, ES, S/L-TR
-subtypes = os.listdir(data_dir+electrophys)
+#subtypes = os.listdir(data_dir+electrophys)
+subtypes = os.listdir(data_dir)
 
 subtype_list = []
 rat_num_list = []
@@ -37,7 +38,8 @@ data_2d = dict()
 
 
 for st in subtypes:
-    all_cells = os.listdir(data_dir+electrophys+"/"+st)
+    #all_cells = os.listdir(data_dir+electrophys+"/"+st)
+    all_cells = os.listdir(data_dir+"/"+st)
     cell_no = []
     rat_no = []
     data_2d[st] = dict()
@@ -53,16 +55,18 @@ for st in subtypes:
                 data_2d[st][rn] = dict()
                 data_2d[st][rn]["name"] = [] 
 
-            if os.path.exists(data_dir+electrophys+"/"+st+"/"+cell+"/"+cell+"_Amp_zscore_2D_OK.csv"):
+            #if os.path.exists(data_dir+electrophys+"/"+st+"/"+cell+"/"+cell+"_Amp_zscore_2D_OK.csv"):
+            if os.path.exists(data_dir+"/"+st+"/"+cell+"/"+cell+"_Amp_zscore_2D_OK.csv"):
                 if cn not in list(data_2d[st][rn].keys()):
                     data_2d[st][rn][cn] = dict()
 
                 #synaptic currents in the map, not zscored
-                data_2d[st][rn][cn]["map_nz"] = pd.read_csv(data_dir+electrophys+"/"+st+"/"+cell+"/"+cell+"_Amp_2D_OK.csv",header=None)
-                data_2d[st][rn][cn]["map"] = pd.read_csv(data_dir+electrophys+"/"+st+"/"+cell+"/"+cell+"_Amp_zscore_2D_OK.csv",header=None)      # Assumption is if rat number is same, at least the cell number is different, that is no duplicate folders for the same rat num and cell number
+                #data_2d[st][rn][cn]["map_nz"] = pd.read_csv(data_dir+electrophys+"/"+st+"/"+cell+"/"+cell+"_Amp_2D_OK.csv",header=None)
+                data_2d[st][rn][cn]["map_nz"] = pd.read_csv(data_dir"/"+st+"/"+cell+"/"+cell+"_Amp_2D_OK.csv",header=None)
+                data_2d[st][rn][cn]["map"] = pd.read_csv(data_dir+"/"+st+"/"+cell+"/"+cell+"_Amp_zscore_2D_OK.csv",header=None)      # Assumption is if rat number is same, at least the cell number is different, that is no duplicate folders for the same rat num and cell number
 		
                 arr =  np.array(data_2d[st][rn][cn]["map"])
-                pos_centered = pd.read_csv(data_dir+electrophys+"/"+st+"/"+cell+"/"+cell+"_Positions_cp_centered_OK.csv",header=None)
+                pos_centered = pd.read_csv(data_dir+"/"+st+"/"+cell+"/"+cell+"_Positions_cp_centered_OK.csv",header=None)
                 ind_contra = np.where(pos_centered<0)[0]
                 ind_ipsi = np.where(pos_centered>0)[0]
                 data_2d[st][rn][cn]["ind_ipsi"] = ind_ipsi
